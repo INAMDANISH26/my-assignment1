77e98fed7a919f0296eae65f371635f28216d3af
INAMDANISH26 <76449079+INAMDANISH26@users.noreply.github.com>
2021-03-08 14:17:07 +0500

    Add files via upload

---
+++ Assignment_1(DSC).py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # TASK-1
+
+# In[21]:
+
+
+num = input('Input:')
+num= int(num)
+if (num % 2) ==0:
+    print('Output: "The number',num,'is even number"')
+else:
+    print('Output: "The number',num,'is odd number"' )
+
+
+# In[22]:
+
+
+num = input('Input:')
+num= int(num)
+if (num % 2) ==0:
+    print('Output: "The number',num,'is even number"')
+else:
+    print('Output: "The number',num,'is odd number"' )
+
+
+# # Task-2
+
+# In[28]:
+
+
+data= '"Hi my name is inam-ur-Rehman and i am currently doing my masters from University of Karachi"'
+print(data)
+data_lowercase= data.lower()
+print(data_lowercase)
+data_uppercase= data.upper()
+print(data_uppercase)
+
